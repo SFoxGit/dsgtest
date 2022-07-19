0ac05b1560ace4a7c56dd50ea6abb64abc805310
SFoxGit <foxeasttrojan06@gmail.com>
2022-07-18 22:09:38 -0400

    export cleanup

--- src/utils/exportAsImage.js
+++ src/utils/exportAsImage.js
@@ -1,12 +1,11 @@
-// import html2canvas from 'html2canvas'
-
 const downloadImage = (blob, fileName) => {
   const fakeLink = window.document.createElement('a')
   fakeLink.style = 'display:none;'
   fakeLink.download = fileName
 
   fakeLink.href = blob
-
+  fakeLink.rel = 'noreferrer'
+  
   document.body.appendChild(fakeLink)
   fakeLink.click()
   document.body.removeChild(fakeLink)
@@ -19,28 +18,3 @@ const exportAsImage = async (imageFileName) => {
 }
 
 export default exportAsImage
-
-// const testFunction = () => {
-//   var sourceimage = document.querySelector('img')
-//   var canvas = document.querySelector('canvas')
-//   var out = document.querySelector('textarea')
-//   var output = document.querySelector('output')
-//   var jpg = document.querySelector('#jpg')
-//   var quality = document.querySelector('#quality')
-//   var context = canvas.getContext('2d')
-//   canvas.style.display = 'none'
-//   function toURL(ev) {
-//     var t = ev.target
-//     if (t.tagName === 'IMG') {
-//       canvas.height = t.offsetHeight
-//       canvas.width = t.offsetWidth
-//       context.drawImage(t, 0, 0, t.offsetWidth, t.offsetHeight)
-//       var url = jpg.checked ? canvas.toDataURL('image/jpeg', +quality.value) : canvas.toDataURL('image/png')
-//       out.value = url
-//       var img = document.createElement('img')
-//       img.setAttribute('src', url)
-//       output.innerHTML = url.length + ' bytes '
-//       output.appendChild(img)
-//     }
-//   }
-// }
